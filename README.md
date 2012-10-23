2401a260aa737deeebfd3021909183ae411c4bad
y-ken <y.ken.studio@gmail.com>
2012-10-23 23:05:43 +0900

    add voice choice function

--- README.md
+++ README.md
@@ -18,6 +18,15 @@ gem install fluent-plugin-boundio
 
 ## Configuration
 
+### Message Format
+`````
+fluent_logger.post('notify.call', {
+  :message  => 'Hello World!',   # Required
+  :number   => '090-1234-5678',  # Optional
+  :voice    => 'male'            # Optional
+})
+`````
+
 ### Sample
 `````
 <source>
@@ -27,10 +36,11 @@ gem install fluent-plugin-boundio
 
 <match notify.call>
   type boundio
-  user_serial_id  YOUR_BOUNDIO_USER_SERIAL_ID
-  api_key         YOUR_BOUNDIO_API_KEY
-  user_key        YOUR_BOUNDIO_USER_KEY
-  default_number  09012345678
+  user_serial_id  BOUNDIO_USER_SERIAL_ID  # Required
+  api_key         BOUNDIO_API_KEY         # Required
+  user_key        BOUNDIO_USER_KEY        # Required
+  default_number  090-1234-5678           # Optional
+  default_voice   female                  # Optional [female/male] (default: female)
   # http://boundio.jp/docs/%E3%83%87%E3%83%99%E3%83%AD%E3%83%83%E3%83%91%E3%83%BC%E3%83%84%E3%83%BC%E3%83%AB
   developer_tool  yes
 </match>
@@ -38,14 +48,14 @@ gem install fluent-plugin-boundio
 
 ### Quick Test
 `````
-# test call to 09012345678 and say "Help! System ABC has down.".
-$ curl http://localhost:8888/notify.call -F 'json={"number":"09012345678","message":"Help! System ABC has down."}'
+# test call to 09012345678 and say "Help! System ABC has down." with male voice.
+$ curl http://localhost:8888/notify.call -F 'json={"number":"09012345678","voice":"male","message":"Help! System ABC has down."}'
 
 # check boundio activity log
 $ tail -f /var/log/td-agent/td-agent.log
 `````
 
-## Example
+## Reference
 
 ### Use Case1: make a call when it goes to over 5 times 500_errors in a minutes.
 http://github.com/y-ken/fluent-plugin-boundio/blob/master/example.conf
--- lib/fluent/plugin/out_boundio.rb
+++ lib/fluent/plugin/out_boundio.rb
@@ -2,10 +2,13 @@
 class Fluent::BoundioOutput < Fluent::Output
   Fluent::Plugin.register_output('boundio', self)
 
+  VOICE_MAP = {'female' => 0, 'male' => 1}
+
   config_param :user_serial_id, :string
   config_param :user_key, :string, :default => nil # Optional at this time
   config_param :api_key, :string
-  config_param :default_number, :string
+  config_param :default_number, :string, :default => nil
+  config_param :default_voice, :string, :default => 'male'
   config_param :developer_tool, :string, :default => 'no'
 
   def initialize
@@ -16,32 +19,35 @@ class Fluent::BoundioOutput < Fluent::Output
 
   def configure(conf)
     super
-    @voice_type = 1
+    unless VOICE_MAP.include?(@default_voice)
+      raise Fluent::ConfigError, "boundio: invalid value for default_voice(male/female): #{@default_voice}"
+    end
     @developer_tool = Fluent::Config.bool_value(@developer_tool) || false
-    $log.info "boundio using developer tool api" if @developer_tool
+    $log.info "boundio: using developer tool api" if @developer_tool
   end
 
   def emit(tag, es, chain)
     es.each do |time,record|
       number = record['number'].nil? ? @default_number : record['number']
-      call(number, record['message'])
+      voice = VOICE_MAP.include?(record['voice']) ? record['voice'] : @default_voice
+      call(number, record['message'], voice)
     end
 
     chain.next
   end
 
-  def call(number, message)
+  def call(number, message, voice = 'male')
     begin
       https = Net::HTTP.new('boundio.jp', 443)
       https.use_ssl = true
-      cast = "file_d(#{message}, #{@voice_type})"
-      query = 'key=' + @api_key + '&tel_to=' + number + '&cast=' + cast
+      cast = "file_d(#{message}, #{VOICE_MAP[voice]})"
+      query = 'key=' + @api_key + '&tel_to=' + number.gsub('-','') + '&cast=' + cast
       path = @developer_tool ? '/api/vd2/' : '/api/v2/'
       response = https.post(path + @user_serial_id + '/call', URI.escape(query))
-      $log.info "boundio makeing a call: #{message} "
-      $log.info "boundio call result: #{response.body}"
+      $log.info "boundio: makeing a call to #{number} #{voice}: #{message} "
+      $log.info "boundio: call result: #{response.body}"
     rescue => e
-      $log.error("boundio Error: #{e.message}")
+      $log.error "boundio: Error: #{e.message}"
     end
   end
 end
